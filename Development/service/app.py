82cb73868c27c04054e282aeaec8279b16574808
Jobey Farias <115730827+Jobeyy@users.noreply.github.com>
2024-09-04 12:54:49 -0500

    Working on hashing and GCS buckets

--- Development/service/app.py
+++ Development/service/app.py
@@ -1,19 +1,18 @@
 from flask import Flask, jsonify
-from flask_cors import CORS
+from flask_cors import CORS # type: ignore
+from ImageToHash import *
+
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/data')
 def index():
-  data = {
-    "name": "John",
-    "age": 30,
-    "city": "New York"
-  }
-  return jsonify(data)
+  #hardcoded directory built for one given image, for test purposes of course
+  file_path = "C:\\Users\\jobey\\Documents\\Final Project 4390\\FinalProject4390\\Development\\service\\images\\concord.png"
+  result = md5checksum(file_path)
+  return jsonify(result)
 
-
-
-
+# Dog Hash: d705ef4a93e84a4ecaf64e8d53531c93 Works
+# Concord Hash: 1fa5bd8494efc127c09c55514b8c668a Works
 if __name__ == '__main__':
   app.run()
