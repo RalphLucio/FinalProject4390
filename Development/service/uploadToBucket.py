6639139d944dbf083ee3c0432d78174f5b23d136
GitDogSpit <ryanwolf21@icloud.com>
2024-10-07 02:33:20 -0500

    Good Fixes

--- Development/service/uploadToBucket.py
+++ Development/service/uploadToBucket.py
@@ -1,27 +1,18 @@
 from google.cloud import storage
-import os
 
-def upload_blob(source_file_name, destination_blob_name):
+def upload_blob(source_file, dest_name):
     '''upload an image to the google bucket'''
-    storage_client = storage.Client.from_service_account_json(r'Development\\service\\meditensor-2178cd54ff6e.json') #grab the stupid credentials itself
-    bucket = storage_client.get_bucket("mediscan-images") #pull the name
-    blob = bucket.blob(destination_blob_name) #What do you want it to be called when its in the bucket?
-    blob.upload_from_filename(source_file_name) #What is it called so I can find it?
-    print(f"File {source_file_name} uploaded to {destination_blob_name}.")
-    return f"UPLOADED!"
+    storage_client = storage.Client.from_service_account_json(r'Development\\service\\meditensor-2178cd54ff6e.json') #LEAVE THIS ALONE. Unless it changes.
+    bucket = storage_client.get_bucket("mediscan-images") #LEAVE THIS ALONE. Unless it changes.
 
-
-def get_latest_file(directory):
-    '''get the most recent file by modification time'''
-    files = os.listdir(directory)
-    full_paths = [os.path.join(directory, f) for f in files]
-    latest_file = max(full_paths, key=os.path.getmtime)  
-    return latest_file
-
+    blob = bucket.blob(dest_name) #What to call it; in the bucket
+    blob.upload_from_filename(source_file) #Upload 'this' file
+    print(f"UPLOAD_BLOB: File {source_file} has been uploaded to bucket as: \"{dest_name}\".")
+    return f"https://storage.cloud.google.com/mediscan-images/{dest_name}"
 
 if __name__ == '__main__':
-    uploads_folder = r'Development\\service\\images\\uploads'   #look in this folder
-    source_file_name = get_latest_file(uploads_folder)  #grab the latest file from it
-    destination_blob_name = os.path.basename(source_file_name) #give it this name
-    file_url = upload_blob(source_file_name, destination_blob_name) #upload it
-    print(f"File uploaded and accessible at: {file_url}")
+    pass
+    #you have to hardcode the file here unless you want to make a helper function, import os
+    # file = DIRECTORY/IMAGE.PNG
+    # what_to_call_it = os.path.basename(file)
+    # file_url = upload_blob(file, what_to_call_it)
